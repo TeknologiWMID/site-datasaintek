564b7fa95c99ef93960d126b1cfe8d0eb56dc5b2
hayuraaa <yunandaharis@gmail.com>
2026-08-20 20:48:03 +0700

    fix favicon: replace Laravel default with Wikimedia icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Data, Sains dan Teknologi Wikimedia Indonesia') }}</title>
 
-        <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/9/96/Ikon_Data_dan_Teknologi_WMID_-_Warna.svg" sizes="any">
-        <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/9/96/Ikon_Data_dan_Teknologi_WMID_-_Warna.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="https://upload.wikimedia.org/wikipedia/commons/9/96/Ikon_Data_dan_Teknologi_WMID_-_Warna.svg">
+        <link rel="icon" href="/favicon.svg" sizes="any">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/favicon.svg">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
